cf866b8b0d529814057b9b8f60b47acb4afd8768
Your Name <you@example.com>
2026-07-13 14:02:07 +0530

    'e-bookstore'

---
+++ App.js
@@ -0,0 +1,22 @@
+console.log(a)
+console.log(b, "priti")
+let a = 10; // 
+const b = 10
+var c = 20 // var is hoisted in to global memory scope and it can be re-declared and re-assigned
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement(
+//     "div",
+//     { id: "child" },
+//     React.createElement("h1", { id: "heading" }, "Namaste React")
+//   ),
+//   React.createElement("div", { id: "child2" }, "Hello World"),
+// ]);
+
+// const heading = React.createElement("h1", { id: "heading" }, "Namaste React");
+
+// console.log(parent); // React.createElement return an object
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(parent);
