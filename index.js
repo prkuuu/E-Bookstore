833908e7f76a0ba05d14c7ded8c75d4ba019f4fd
Priti Dixit <pritigupta.0911@gmail.com>
2026-07-16 13:29:45 +0530

    update main

--- index.js
+++
@@ -1,6 +0,0 @@
-import { createRoot } from "react-dom/client";
-import ReactDOM from 'react-dom/client'
-import App from "./App";
-
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
